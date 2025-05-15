8280a14fec925ef95a9de8a53dcf72154c83aaa3
hafiz-muhammed <muhammedhafizkv777@gmail.com>
2025-05-15 13:15:06 +0530

    update

--- Frontend/src/components/Dashboard.jsx
+++ Frontend/src/components/Dashboard.jsx
@@ -9,7 +9,8 @@ const Dashboard = () => {
     <div className="min-h-screen flex items-center justify-center bg-gray-50">
       <div className="bg-white shadow-lg rounded-xl p-8 w-full max-w-md text-center">
         <h1 className="text-3xl font-semibold text-gray-800 mb-4">
-          Welcome, {user?.email}
+          Welcome<br/>
+         {user?.name}
         </h1>
         <p className="text-gray-600">You have successfully logged in.</p>
       </div>
